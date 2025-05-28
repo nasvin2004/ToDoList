d432fc06a6310848ca7ea39c117e30aaedab49e6
nasvin2004 <nasvin22@gmail.com>
2025-05-28 17:45:07 +0530

    facebook-resolve

--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -49,7 +49,7 @@ passport.use(new GoogleStrategy({
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "https://todolist-backend-app.onrender.com/auth/github/callback"
+  callbackURL: "https://todolist-backend-jdwc.onrender.com/auth/github/callback"
 }, (accessToken, refreshToken, profile, done) => {
   handleUser(profile, 'github', done);
 }));
