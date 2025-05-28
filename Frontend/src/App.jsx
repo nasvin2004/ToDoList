3ca60d4a75b2f76c3de2b7914a27703d76f150db
nasvin2004 <nasvin22@gmail.com>
2025-05-28 21:15:36 +0530

    protected-route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SocialLogin from "./Component/SocialLogin";
 import SocialAuthSuccess from "./Component/SocialAuthSuccess";
 import Dashboard from "./Component/Dashboard";
+import ProtectedRoute from "./Component/ProtectedRoute"; // Import here
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -11,10 +12,16 @@ function App() {
       <Routes>
         <Route path="/" element={<SocialLogin />} />
         <Route path="/social-auth-success" element={<SocialAuthSuccess />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
 
-      {/* Add ToastContainer once at app level */}
       <ToastContainer />
     </BrowserRouter>
   );
