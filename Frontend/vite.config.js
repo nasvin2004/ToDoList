2b15c6f61c395fbcc04dcb1ffdbc6ddbddbf4e37
nasvin2004 <nasvin22@gmail.com>
2025-05-28 12:59:46 +0530

    new Commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
-  root: 'frontend', // 👈 tells Vite to start from frontend/
   plugins: [react()],
-  build: {
-    outDir: '../dist',     // 👈 output to project-root/dist
-    emptyOutDir: true,
-  },
 })
+
