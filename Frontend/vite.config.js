10f5227e1a009395defe57217c260ce90247a7b4
nasvin2004 <nasvin22@gmail.com>
2025-05-28 12:47:53 +0530

    popper-2

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: 'frontend', // 👈 tells Vite to start from frontend/
   plugins: [react()],
+  build: {
+    outDir: '../dist',     // 👈 output to project-root/dist
+    emptyOutDir: true,
+  },
 })
-
